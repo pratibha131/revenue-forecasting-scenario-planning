46acf8bea219242a050696526db4623706e414ef
pratibha131 <mehtapratibha540@gmail.com>
2026-01-05 17:12:43 +0530

    frontend & backend cors

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from src.api.routes import api_blueprint
 from src.modeling.prophet_model import load_and_train_model
 
 def create_app():
     app = Flask(__name__)
 
+    CORS(app)
     # Load model ONCE at startup
     load_and_train_model(
         data_path=r"C:\Users\Pratibha\OneDrive\Desktop\DATA SCIENCE RESOURCES\revenue-forecasting-scenario-planning\revenue-forecasting-scenario-planning\data\processed\monthly_revenue_processed_ALL_YEARS_CORRECT.csv"
